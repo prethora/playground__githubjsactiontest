49a59896c47ebded3bc558ae6f7e0aa23d70f574
Dominique Adolphe <domiado16@gmail.com>
2021-10-10 12:45:19 +0400

    2021-10-10-12-45

--- dist/index.js
+++ dist/index.js
@@ -8448,12 +8448,22 @@ async function run()
 {
     const token = core.getInput("token");
     const octokit = github.getOctokit(token);        
-    const res = await octokit.rest.repos.getContent({
-        owner: github.context.payload.repository.owner.name,
-        repo: github.context.payload.repository.name,
-        path: "package.json",
-    });
-    console.log(res);
+    const { ref,repository: { owner: { name: owner }, name: repo } } = github.context.payload;
+
+    if ((ref) && (ref.startsWith("refs/tags/")))
+    {
+        const tag_name = ref.substr(10);
+        const res = await octokit.rest.repos.createRelease({
+            owner,
+            repo,
+            tag_name,
+        });
+        console.log(res);
+    }
+    else
+    {
+        throw "ref must be a tag";
+    }
 }
 
 run();
