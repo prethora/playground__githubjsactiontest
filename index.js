e89454ee02ab2b46e42c1dd98a13a82125af1217
Dominique Adolphe <domiado16@gmail.com>
2021-10-10 11:57:46 +0400

    2021-10-10-11-57

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const github = require('@actions/github');
 async function run()
 {
     const token = core.getInput("token");
-    const octokit = github.getOctokit(token);
+    const octokit = github.getOctokit(token);        
     const res = await octokit.rest.repos.getContent({
-        owner: "prethora",
-        repo: "playground__githubjsactiontest",
-        path: "action.yml",
+        owner: github.context.payload.repository.owner.name,
+        repo: github.context.payload.repository.name,
+        path: "package.json",
     });
     console.log(res);
 }
